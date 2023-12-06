f05522b534ba02d5b27ab3d1ace0c59bf3486541
Daan <90244163+DaanMth@users.noreply.github.com>
2023-12-06 16:11:00 +0100

    Created different cultures

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import io
 import base64
 from PIL import Image
 import time
+import os
 
 url = "http://127.0.0.1:7860"
 
 payload = {
-    "prompt": "<lora:Better Portrait Lighting:1>A black and white picture of a man with a smooth white background",
+    "prompt": "<lora:Better Portrait Lighting:1>A black and white picture of a man with different cultural backgrounds with a smooth white background",
     "steps": 20
 }
 
 i=2
+x=2
 
 while True:
     response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
@@ -21,4 +23,8 @@ while True:
     image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
     image.save('./images/output' + str(i) + '.png' )
     i = i + 1
-    time.sleep(15)
+    if i >= 7:
+        os.remove('./images/output' + str(x) + '.png' )
+        x = x + 1
+
+    time.sleep(20)
